eddb005ee43d4f39b315bf77b62303e0432d2b82
Suzuki Yoshimi <suzukiyo@am1.sega.co.jp>
2016-09-05 14:59:06 +0900

    add UnitTest derectory

---
+++ test/LWCollideTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using LWCollide.Core;
+
+namespace LWCollide
+{
+    namespace UnitTest
+    {
+        public class LWCollideTest
+        {
+            public static void Main()
+            {
+                new LWCollideTest().Run();
+            }
+
+            public void Run()
+            {
+
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
